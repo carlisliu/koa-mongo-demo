9962134b3b66a14cdf4bea2641cf7a3957392ba5
Carlis Liu <carlisliu@live.com>
2017-08-08 20:13:44 +0800

    fix deps

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')();
-const ActivitySchema = require('./services/activity');
+const ActivitySchema = require('../services/activity');
 
 router.get('/', async function(ctx, next) {
     let activity = await ActivitySchema.find({}).skip(1).limit(10).sort('-create_date');
